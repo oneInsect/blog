790ca91dde69db41dca6659f3bd938d2138d6a83
Lance <xfeng_insect@foxmail.com>
2019-11-22 21:35:52 +0800

    add article models

--- blog/src/article/models.py
+++ blog/src/article/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# 导入Django自带用户模块
+
+# 文章分类
+class Category(models.Model):
+    name = models.CharField('博客分类', max_length=100)
+    index = models.IntegerField(default=999, verbose_name='分类排序')
+
+    class Meta:
+        verbose_name = '博客分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 文章标签
+class Tag(models.Model):
+    name = models.CharField('文章标签', max_length=100)
+
+    class Meta:
+        verbose_name = '文章标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+#  TODO  点赞 收藏
+
+
+
+# 文章
+class Article(models.Model):
+    title = models.CharField('标题', max_length=70)
+    excerpt = models.TextField('摘要', max_length=200, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING,
+                                 verbose_name='分类', blank=True, null=True)
+    # 使用外键关联分类表与分类是一对多关系
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    # 使用外键关联标签表与标签是多对多关系
+    img = models.ImageField(upload_to='article_img/%Y/%m/%d/',
+                            verbose_name='文章图片', blank=True, null=True)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+    """
+    文章作者，这里User是从django.contrib.auth.models导入的。
+    这里我们通过 ForeignKey 把文章和 User 关联了起来。
+    """
+    views = models.PositiveIntegerField('阅读量', default=0)
+    created_time = models.DateTimeField('发布时间', auto_now_add=True)
+    modified_time = models.DateTimeField('修改时间', auto_now=True)
+
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+
+
+    def __str__(self):
+        return self.title
+
+
+# TODO 评论
+# TODO 图片
--- blog/src/utils/middleware.py
+++ blog/src/utils/middleware.py
@@ -1,10 +1,13 @@
 import json
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
+from .tools import BaseResponse
 
 
 class GlobalException(MiddlewareMixin):
 
     def process_exception(self, _, exception):
-        return HttpResponse(content=json.dumps({"msg": str(exception)}),
-                            status=500)
+        rep = BaseResponse()
+        rep.error = str(exception)
+        rep.code = 500
+        return JsonResponse(rep.__dict__)
